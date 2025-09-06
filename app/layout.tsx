1726f03ed9b52dfc9647930cf36bfc11a4647fc6
Вікторія Плакуща <dneprovaviktoria@gmail.com>
2025-09-06 19:30:38 +0300

    initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,21 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "NoteHub",
   description: "Simple notes app created with Next.js",
+  openGraph: {
+    title: "NoteHub",
+    description: "Simple notes app created with Next.js",
+    url: "",
+    siteName: "NoteHub",
+    images: [
+      {
+        url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
+        width: 1200,
+        height: 630,
+        alt: "NoteHub Image",
+      },
+    ],
+    type: "website",
+  },
 };
 
 export default function RootLayout({
